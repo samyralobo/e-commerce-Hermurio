58ba4142fd608d88964189d1f0ac116d2ddd76af
samyralobo <samylobosi@gmail.com>
2025-11-03 15:40:40 -0300

    subindo base do projeto

---
+++ src/main/java/com/ecommerce/Hermurio/HermurioApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Hermurio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermurioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermurioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/Hermurio/HermurioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Hermurio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermurioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
